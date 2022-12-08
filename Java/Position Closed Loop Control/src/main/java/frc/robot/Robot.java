2ac6d13b08fa9de4bc6061943be77a5c28365675
Techmj <mrenaaljain@gmail.com>
2022-12-07 23:59:30 -0500

    initial commit

--- Java/Position Closed Loop Control/src/main/java/frc/robot/Robot.java
+++ Java/Position Closed Loop Control/src/main/java/frc/robot/Robot.java
@@ -16,7 +16,7 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends TimedRobot {
-  private static final int deviceID = 1;
+  private static final int deviceID = 3;
   private CANSparkMax m_motor;
   private SparkMaxPIDController m_pidController;
   private RelativeEncoder m_encoder;
@@ -28,14 +28,17 @@ public class Robot extends TimedRobot {
     m_motor = new CANSparkMax(deviceID, MotorType.kBrushless);
 
     /**
-     * The restoreFactoryDefaults method can be used to reset the configuration parameters
-     * in the SPARK MAX to their factory default state. If no argument is passed, these
+     * The restoreFactoryDefaults method can be used to reset the configuration
+     * parameters
+     * in the SPARK MAX to their factory default state. If no argument is passed,
+     * these
      * parameters will not persist between power cycles
      */
     m_motor.restoreFactoryDefaults();
 
     /**
-     * In order to use PID functionality for a controller, a SparkMaxPIDController object
+     * In order to use PID functionality for a controller, a SparkMaxPIDController
+     * object
      * is constructed by calling the getPIDController() method on an existing
      * CANSparkMax object
      */
@@ -45,12 +48,12 @@ public class Robot extends TimedRobot {
     m_encoder = m_motor.getEncoder();
 
     // PID coefficients
-    kP = 0.1; 
+    kP = 0.1;
     kI = 1e-4;
-    kD = 1; 
-    kIz = 0; 
-    kFF = 0; 
-    kMaxOutput = 1; 
+    kD = 1;
+    kIz = 0;
+    kFF = 0;
+    kMaxOutput = 1;
     kMinOutput = -1;
 
     // set PID coefficients
@@ -84,33 +87,50 @@ public class Robot extends TimedRobot {
     double min = SmartDashboard.getNumber("Min Output", 0);
     double rotations = SmartDashboard.getNumber("Set Rotations", 0);
 
-    // if PID coefficients on SmartDashboard have changed, write new values to controller
-    if((p != kP)) { m_pidController.setP(p); kP = p; }
-    if((i != kI)) { m_pidController.setI(i); kI = i; }
-    if((d != kD)) { m_pidController.setD(d); kD = d; }
-    if((iz != kIz)) { m_pidController.setIZone(iz); kIz = iz; }
-    if((ff != kFF)) { m_pidController.setFF(ff); kFF = ff; }
-    if((max != kMaxOutput) || (min != kMinOutput)) { 
-      m_pidController.setOutputRange(min, max); 
-      kMinOutput = min; kMaxOutput = max; 
+    // if PID coefficients on SmartDashboard have changed, write new values to
+    // controller
+    if ((p != kP)) {
+      m_pidController.setP(p);
+      kP = p;
+    }
+    if ((i != kI)) {
+      m_pidController.setI(i);
+      kI = i;
+    }
+    if ((d != kD)) {
+      m_pidController.setD(d);
+      kD = d;
+    }
+    if ((iz != kIz)) {
+      m_pidController.setIZone(iz);
+      kIz = iz;
+    }
+    if ((ff != kFF)) {
+      m_pidController.setFF(ff);
+      kFF = ff;
+    }
+    if ((max != kMaxOutput) || (min != kMinOutput)) {
+      m_pidController.setOutputRange(min, max);
+      kMinOutput = min;
+      kMaxOutput = max;
     }
 
     /**
-     * PIDController objects are commanded to a set point using the 
+     * PIDController objects are commanded to a set point using the
      * SetReference() method.
      * 
      * The first parameter is the value of the set point, whose units vary
      * depending on the control type set in the second parameter.
      * 
-     * The second parameter is the control type can be set to one of four 
+     * The second parameter is the control type can be set to one of four
      * parameters:
-     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
-     *  com.revrobotics.CANSparkMax.ControlType.kPosition
-     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
-     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+     * com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+     * com.revrobotics.CANSparkMax.ControlType.kPosition
+     * com.revrobotics.CANSparkMax.ControlType.kVelocity
+     * com.revrobotics.CANSparkMax.ControlType.kVoltage
      */
     m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
-    
+
     SmartDashboard.putNumber("SetPoint", rotations);
     SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
   }
